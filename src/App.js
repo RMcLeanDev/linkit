fd102aa636e8b912f43ffc18d0bf6f1bf90f8f49
RMcLeanDev <rmcleandev@gmail.com>
2023-01-26 14:09:10 -0800

    upgrade react dom to react 18 commands

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {testFunction} from './actions/index';
 function App() {
   return (
     <div className="App">
-      <h1>Click the button:</h1>
+      <h1>Click the button: </h1>
       <button onClick={() => store.dispatch(testFunction())}>TEST</button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
@@ -10,24 +9,16 @@ import rootReducer from './reducers';
 import {Provider} from 'react-redux';
 import './actions';
 
+import {createRoot} from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 export {store};
 
-const render = (Component) => {
-  ReactDOM.render(
-    <HashRouter>
+root.render(
+  <HashRouter>
       <Provider store={store}>
-        <Component />
+        <App />
       </Provider>
-    </HashRouter>,
-    document.getElementById('root')
-  );
-}
-
-render(App);
-
-if (module.hot) {
-  module.hot.accept('./App', () => {
-    render(App);
-  });
-}
+    </HashRouter>
+)
