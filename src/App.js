c819640c1cb5a34b0daa28794ee39d8ce374081b
RMcLeanDev <rmcleandev@gmail.com>
2023-01-26 15:10:08 -0800

    add footer component and basic styling

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.scss';
 import { store } from './';
 import {testFunction} from './actions/index';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer />
       <h1>Click the button: </h1>
       <button onClick={() => store.dispatch(testFunction())}>TEST</button>
     </div>
