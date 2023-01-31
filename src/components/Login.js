41a2abd1a3544866c8cd14c080ba2fd7fca38b43
RMcLeanDev <rmcleandev@gmail.com>
2023-01-31 07:15:18 -0800

    add toggle to show/hide password field

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,29 @@
 import React, {useState} from "react";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
 function Login(){
 
     const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [viewPass, setViewPass] = useState(true);
 
     function login(e){
         e.preventDefault();
-        console.log("will be able to sign in one day")
+        console.log(email)
+        /* firebase.auth().signInWithEmailAndPassword(email, password).catch(err => {
+            console.log(err)
+        }) */
     }
     return(
         <div className="loginComponent">
             <img src={require('../assets/LinkITLogoFull.png')}/>
             <div className="loginContainer">
                 <form onSubmit={login}>
-                    <input placeholder="Email"/>
+                    <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"/>
                     <br/>
-                    <input placeholder="Password"/>
+                    <input type={viewPass ? "password":"text"} value={password} onChange={e => setPassword(e.target.value)} placeholder="Password"/>
+                    <i className={viewPass ? "far fa-eye-slash": "far fa-eye"} id={viewPass ? "natogglePassword" : "togglePassword"} onClick={() => setViewPass(!viewPass)}></i>
                     <br/>
                     <button type="submit">Sign In</button>
                 </form>
