16861fee686ff11cce4b31b1f805224d53512b99
RMcLeanDev <rmcleandev@gmail.com>
2023-02-08 10:43:22 -0800

    build out base for showing support requests

--- src/components/SupportRequest.js
+++ src/components/SupportRequest.js
@@ -2,9 +2,28 @@ import React from 'react'
 
 function SupportRequest(){
     return(
-        <div>
-            <p>create new ticket</p>
-            <p>current support tickets</p>
+        <div className="supportRequestContainer">
+            <h1>Current Requests:</h1>
+            <div className="ticketsContainer">
+            <i class="fa-solid fa-plus addNewTicket"></i>
+                <div className="currentTickets">
+                    <div className="supportItem">
+                        <p>name</p>
+                        <p>severity</p>
+                        <p>status</p>
+                    </div>
+                    <div className="supportItem">
+                        <p>Ryan</p>
+                        <p>low</p>
+                        <p>ongoing</p>
+                    </div>
+                    <div className="supportItem">
+                        <p>Ryan</p>
+                        <p>low</p>
+                        <p>ongoing</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
