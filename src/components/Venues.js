2d99ff68e9bd50bfae08c2e313fef3ae345d3bc0
RMcLeanDev <rmcleandev@gmail.com>
2023-03-02 08:52:39 -0800

    add search functionality to venues

--- src/components/Venues.js
+++ src/components/Venues.js
@@ -5,19 +5,22 @@ import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 function Venues(props){
 
     const [venueForm, setVenueForm] = useState(false);
+    const [search, setSearch] = useState('');
+
     console.log(props)
     return(
         <div className="venuesContainer">
             {venueForm ? (disableBodyScroll(document), <AddVenueForm closeWindow={() => setVenueForm(false)}/>):(enableBodyScroll(document), null)}
-            <input className="searchBar" placeholder="SEARCH" />
+            <input className="searchBar" placeholder="SEARCH" onChange={e => setSearch(e.target.value)}/>
             <i className="fa-solid fa-plus addNewTicketButton" style={{color: "#c54dff"}} onClick={() => setVenueForm(true)}/>
             <div>
                 {props.venues ? Object.keys(props.venues).map(venues => {
                     let venue = props.venues[venues]
                     return(
+                        venue.venueName.includes(search) ? 
                         <div key={venues}>
                             <h1>{venue.venueName}</h1>
-                        </div>
+                        </div> : null
                     )
                 }) : "loading..."}
             </div>
