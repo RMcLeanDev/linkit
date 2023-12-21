f26697d402407c61d849e286cb70c5b2b72ddbc8
RMcLeanDev <rmcleandev@gmail.com>
2023-12-21 11:42:18 -0800

    attempting to get user by uid in firebase

--- src/components/NoteItems.js
+++ src/components/NoteItems.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import firebase from 'firebase/compat/app';
+import "firebase/compat/auth";
+import "firebase/compat/database";
 
 function NoteItems(props){
 
-    console.log(props)
+    const [noteForm, setNoteForm] = useState(true);
+    const [textArea, setTextArea] = useState('')
+    
+    firebase.auth()
+
+    function submitNote(){
+        let obj = {[Date.now()]: {userSubmited: firebase.auth().currentUser.uid, dateSubmited: Date.now(), note: textArea}};
+        console.log(obj)
+        if(props.info.venueName){
+            firebase.database().ref(`venues/${props.info.id}/notes`).update(obj)
+        } else {
+            console.log("no")
+        }
+    }
+
     return(
         <div className="notesContainer">
             <h1>Notes</h1>
-            <button>New Note +</button>
+            <button className="newNote" onClick={() => setNoteForm(!noteForm)}>New Note +</button>
             <hr/>
+            {noteForm ? <div className={noteForm ? "showGeneral":"hideGeneral"}>
+                <textarea style={{width: "99%", height: "100px"}} value={textArea} onChange={(e) => setTextArea(e.target.value)}/>
+                <button onClick={submitNote}>Submit</button>
+                <hr/>
+            </div>:null}
             {props.info.notes ? <div>
                 <p>hhello</p>
             </div>:"No Notes have been created"}
