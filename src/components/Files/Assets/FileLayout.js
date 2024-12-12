6e5aba27256211926c01b3e17e3d07f1e20b1118
Ryan McLean <45721485+RMcLeanDev@users.noreply.github.com>
2024-12-12 14:51:00 -0800

    updates files layout
    updated files component and files layout component to display any files a user has uploaded. add information to each file to display

--- src/components/Files/Assets/FileLayout.js
+++ src/components/Files/Assets/FileLayout.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { FaRegImage, FaRegFileVideo } from "react-icons/fa";
+import moment from 'moment'
 
-function FileLayout(){
+function FileLayout({file}){
+    console.log(file)
     return(
         <div className="fileLayout">
-            file layout
+            <div className="fileImage">
+                <img src={file.url}/>
+            </div>
+            <p className="fileName">{file.originalName}</p>
+            <p className="fileIcon">{file.fileType.includes("image") ? <FaRegImage color="#404040"/> : <FaRegFileVideo color="#404040"/>}</p>
+            <p className="fileAdded">Last Modified: {moment(file.modifiedAt).startOf('hour').fromNow()}</p>
         </div>
     )
 }
--- src/components/Files/Assets/Files.js
+++ src/components/Files/Assets/Files.js
@@ -1,17 +1,35 @@
-import React from 'react'
-import FileUpload from './FileUpload';
-import FileLayout from './FileLayout';
+import React from "react";
+import { connect } from "react-redux";
+import FileUpload from "./FileUpload";
+import FileLayout from "./FileLayout";
 
-function Files(){
+function Files({ userInfo, isLoading }) {
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading Files...</h1>
+      </div>
+    );
+  }
+  console.log(userInfo);
 
-    
-    return(
-        <div>
-            <h1>Files Component</h1>
-            <FileUpload />
-            <FileLayout />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Files Component</h1>
+      <FileUpload userId={userInfo?.userid} /> 
+      <div className="displayFiles">
+        {Object.keys(userInfo.files).map((file) => {
+            let currentFile = userInfo.files[file]
+            return <FileLayout file={currentFile}/>
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Files;
+const mapStateToProps = (state) => ({
+  userInfo: state.userInfoState, 
+  isLoading: state.filesState === null || state.userInfoState === null, 
+});
+
+export default connect(mapStateToProps)(Files);
