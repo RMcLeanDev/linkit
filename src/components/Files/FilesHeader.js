ae7e7267c1ffc562a576b37bcb63776556ed0537
Ryan McLean <45721485+RMcLeanDev@users.noreply.github.com>
2025-01-03 08:58:01 -0800

    files header
    adding more styling and creating the files header.

--- src/components/Files/FilesHeader.js
+++ src/components/Files/FilesHeader.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FaFilter } from "react-icons/fa";
 
 function FilesHeader(props){
+
+    const [filterDropdown, setFilterDropdown] = useState(true);
+
     return(
         <div className="filesHeader">
             header
-            <FaFilter className="filterIcon"/>
+            <div className="filterSection">
+                <FaFilter className="filterIcon"/>
+                {filterDropdown ? 
+                <div className="filterOptions">
+                    options
+                </div>
+                : null}
+            </div>
         </div>
     )
 }
